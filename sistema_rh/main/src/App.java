93d40616dbd056891e48e52804a5b6b66de5975b
Gustavo Inacio <gustavo0207yt@gmail.com>
2026-02-11 14:38:54 -0300

    feat: sistema de RH inicial com processamento de cargos e bonus via arrays

---
+++ sistema_rh/main/src/App.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int cod, hr;
+        double pag;
+        String tab_cod[] = { "CEO", "Gerente", "Supervisor", "Coordenador", "Balconista", "Vendedor",
+                "Auxiliar de Limpeza", "Jovem Aprendiz" };
+
+        IO.print("Digite seu codigo de funcionario: ");
+        cod = sc.nextInt();
+        IO.print("Digite a quantidade de horas trabalhadas: ");
+        hr = sc.nextInt();
+        sc.close();
+        if (cod < 4) {
+            pag = 5.50 + (40 * 5.50) / 100;
+        } else {
+            pag = 5.50;
+        }
+        ;
+
+        if (cod <= 7) {
+            if (cod <= 4) {
+                pag = 5.50 + (40 * 5.50) / 100;
+            } else {
+                pag = 5.50;
+            };
+
+            System.out.printf("Olá " + tab_cod[cod] + " o valor a receber é de: " + hr * pag);
+        } else {
+            IO.println("Codigo de Funcionario Inexistente");
+        }
+        ;
+
+    }
+}
